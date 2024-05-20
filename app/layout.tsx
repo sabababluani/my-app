bad9d0edc2eac6f42d0e4ad477844ce03fcb7247
Saba <sababablo@gmail.com>
2024-05-20 12:48:26 +0400

    set new favicon on website

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="/google.png" />
-      </Head>
+      <head>
+        <link rel="shortcut icon" href="/google.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
