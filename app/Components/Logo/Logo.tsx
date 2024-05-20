ee9c393d359010d95cc723e5042d0f64fa27943b
Saba <sababablo@gmail.com>
2024-05-20 13:30:35 +0400

    made main input responsive

--- app/Components/Logo/Logo.tsx
+++ app/Components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "./Logo.module.scss"
 export default function Logo() {
     return (
         <div className={Image.container}>
-            <img src="/GoogleLogo.webp" alt="Google logo" />
+            <img src="/GoogleLogo.webp" alt="Google logo" width={300} height={100} />
         </div>
     )
 }   
