adcafdad10d851b0d8f4b5537265d8362039a791
Saba <sababablo@gmail.com>
2024-06-13 20:00:14 +0400

    same

--- app/Tasks/page.tsx
+++ app/Tasks/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useEffect, useState } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { useState } from 'react';
 import styles from './page.module.scss'
+import { Droppable, Draggable, DragDropContext } from 'react-beautiful-dnd';
+
 
 const DATA = [
     {
@@ -34,74 +35,64 @@ const DATA = [
             { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Hammer" },
         ],
         tint: 3,
-    },
+    }
 ];
 
-function App() {
+
+const Tasks = () => {
+
     const [stores, setStores] = useState(DATA);
 
-    const handleDragAndDrop = (results: any) => {
+
+    const handleDragDrop = (results: any) => {
         const { source, destination, type } = results;
 
         if (!destination) return;
 
-        if (
-            source.droppableId === destination.droppableId &&
-            source.index === destination.index
-        )
-            return;
+        if (source.droppableId === destination.droppableId && source.index === destination.index) return;
 
-        if (type === "group") {
+        if (type === 'group') {
             const reorderedStores = [...stores];
 
-            const storeSourceIndex = source.index;
-            const storeDestinatonIndex = destination.index;
-
-            const [removedStore] = reorderedStores.splice(storeSourceIndex, 1);
-            reorderedStores.splice(storeDestinatonIndex, 0, removedStore);
-
+            const sourceIndex = source.index;
+            const destinationIndex = destination.index;
+            const [removedStore] = reorderedStores.splice(sourceIndex, 1);
+            reorderedStores.splice(destinationIndex, 0, removedStore);
             return setStores(reorderedStores);
         }
 
-        
-        const itemSourceIndex = source.index;
-        const itemDestinationIndex = destination.index;
-
-        const storeSourceIndex = stores.findIndex(
-            (store) => store.id === source.droppableId
-        );
-        const storeDestinationIndex = stores.findIndex(
-            (store) => store.id === destination.droppableId
-        );
-
+        const storeSourceIndex = stores.findIndex((store) => store.id === source.droppableId);
+        const storeDestinationIndex = stores.findIndex((store) => store.id === destination.droppableId);
         const newSourceItems = [...stores[storeSourceIndex].items];
         const newDestinationItems =
             source.droppableId !== destination.droppableId
                 ? [...stores[storeDestinationIndex].items]
                 : newSourceItems;
 
-        const [deletedItem] = newSourceItems.splice(itemSourceIndex, 1);
-        newDestinationItems.splice(itemDestinationIndex, 0, deletedItem);
+
+        const [deletedItem] = newSourceItems.splice(source.index, 1);
+        newDestinationItems.splice(destination.index, 0, deletedItem);
 
         const newStores = [...stores];
 
         newStores[storeSourceIndex] = {
             ...stores[storeSourceIndex],
             items: newSourceItems,
-        };
-        newStores[storeDestinationIndex] = {
-            ...stores[storeDestinationIndex],
-            items: newDestinationItems,
-        };
+        }
 
-        setStores(newStores);
-    };
+        newStores[storeDestinationIndex] = {
+            ...stores[storeSourceIndex],
+            items: newDestinationItems,
+        }
+
+        setStores(newStores)
+    }
 
     return (
         <div className={styles.page}>
-            <div className={styles.layout__wrapper}>
+            <div className={styles.layoutWrapper}>
                 <div className={styles.card}>
-                    <DragDropContext onDragEnd={handleDragAndDrop}>
+                    <DragDropContext onDragEnd={handleDragDrop}>
                         <div className={styles.header}>
                             <h1>Shopping List</h1>
                         </div>
@@ -132,13 +123,14 @@ function App() {
                     </DragDropContext>
                 </div>
             </div>
-        </div>
-    );
+        </div >
+    )
 }
+
 
 function StoreList({ name, items, id }: any) {
     return (
-        <Droppable droppableId={id}>
+        <Droppable droppableId={id} >
             {(provided) => (
                 <div {...provided.droppableProps} ref={provided.innerRef}>
                     <div className={styles.storeContainer}>
@@ -146,25 +138,22 @@ function StoreList({ name, items, id }: any) {
                     </div>
                     <div className={styles.itemsContainer}>
                         {items.map((item: any, index: any) => (
-                            <Draggable draggableId={item.id} index={index} key={item.id}>
+                            <Draggable draggableId={item.id}
+                                index={index}
+                                key={item.id}>
                                 {(provided) => (
-                                    <div
-                                        className={styles.itemContainer}
-                                        {...provided.dragHandleProps}
-                                        {...provided.draggableProps}
-                                        ref={provided.innerRef}
-                                    >
+                                    <div className={styles.itemContainer} {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef}>
                                         <h4>{item.name}</h4>
                                     </div>
                                 )}
                             </Draggable>
                         ))}
-                        {provided.placeholder}
                     </div>
+                    {provided.placeholder}
                 </div>
             )}
         </Droppable>
-    );
+    )
 }
 
-export default App;
+export default Tasks;
