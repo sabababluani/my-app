12ec815e6130c47ce3665b95cd1cd270e9e55079
sabababluani <158265615+sabababluani@users.noreply.github.com>
2024-06-19 21:33:40 +0400

    Added register function

--- app/Components/LogIn/LogIn.tsx
+++ app/Components/LogIn/LogIn.tsx
@@ -1,36 +1,60 @@
 import React, { useState } from 'react';
 import type { FormProps } from 'antd';
-import { Button, Checkbox, Form, Input, Modal } from 'antd';
+import { Button, Form, Input, Modal } from 'antd';
 import { useRecoilState } from 'recoil';
 import { userNameState } from '@/app/atoms/states';
-import styles from './Login.module.scss'
+import styles from './Login.module.scss';
 import Link from 'next/link';
+import Register from '../Register/Register';
 
 interface Props {
     onLoginSuccess: (username: string) => void;
 }
+
 const Login = (props: Props) => {
-
     const [userName, setUserName] = useRecoilState(userNameState);
+    const [userWantsRegister, setUserWantsRegister] = useState(false);
+    const [open, setOpen] = useState(false);
 
-    const onFinish: FormProps<any>['onFinish'] = (values: any) => {
-        setOpen(false);
-        setUserName(values.username);
-        props.onLoginSuccess(values.username);
+    const onSuccesfulRegister = () => {
+        setUserWantsRegister(false);
     };
 
-    const [open, setOpen] = useState(false);
+    const onRegisterClick = () => {
+        setUserWantsRegister(!userWantsRegister);
+    };
+
+    const onFinish: FormProps<any>['onFinish'] = (values: any) => {
+        const storedUsername = localStorage.getItem('username');
+        const storedPassword = localStorage.getItem('password');
+
+        if (values.username === storedUsername && values.password === storedPassword) {
+            setOpen(false);
+            setUserName(values.username);
+            props.onLoginSuccess(values.username);
+        } else {
+            alert('Invalid username or password');
+        }
+    };
+
     const showModal = () => {
         setOpen(true);
     };
+
     const handleOk = () => {
         setOpen(false);
     };
+
     const handleCancel = () => {
         setOpen(false);
     };
+
+    const onLoginClick = () => {
+        setUserWantsRegister(false);
+    };
+
     return (
-        <div>
+        <>
             <Button type="primary" onClick={showModal}>
                 Sign in
             </Button>
@@ -39,40 +63,47 @@ const Login = (props: Props) => {
                 onCancel={handleCancel}
                 onOk={handleOk}
             >
-                <Form
-                    name="basic"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}
-                    style={{ maxWidth: 600, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                    autoComplete="off"
-                >
-                    <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
+                {!userWantsRegister ? (
+                    <Form
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        style={{ maxWidth: 600, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        autoComplete="off"
                     >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                    >
-                        <Input.Password />
-                    </Form.Item>
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                        <Button type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                    </Form.Item>
-                    <div className={styles.createContainer}>
-                        <Link href ="/Register" >Create New Account</Link>
-                    </div>
-                </Form>
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                        <div className={styles.createContainer}>
+                            <Link href="#" onClick={onRegisterClick}>
+                                Create New Account
+                            </Link>
+                        </div>
+                    </Form>
+                ) : (
+                    <Register onLoginClick={onLoginClick} onSuccesfulRegister={onSuccesfulRegister} />
+                )}
             </Modal>
-        </div>
+        </>
     );
 };
+
 export default Login;
--- app/Components/Register/Register.tsx
+++ app/Components/Register/Register.tsx
@@ -1,32 +1,98 @@
-import React, { useState } from 'react';
-import { Modal } from 'antd';
+import React from 'react';
+import type { FormProps } from 'antd';
+import { Button, Form, Input } from 'antd';
+import Link from 'next/link';
+import styles from './Register.module.scss';
 
+type FieldType = {
+    username?: string;
+    password?: string;
+    confirmPassword: string;
+    remember?: boolean;
+};
 
 interface Props {
-    onLoginSuccess: (username: string) => void;
+    onLoginClick: () => void;
+    onSuccesfulRegister: () => void;
 }
-const Register = (props: Props) => {
 
-    const [open, setOpen] = useState(false);
-    const showModal = () => {
-        setOpen(true);
+const Register = ({ onLoginClick, onSuccesfulRegister }: Props) => {
+    const onFinish: FormProps<FieldType>['onFinish'] = (registerValue) => {
+        console.log('Success:', registerValue);
+
+        if (registerValue && registerValue.username) {
+            localStorage.setItem('username', registerValue.username)
+        }
+
+        if (registerValue && registerValue.password) {
+            localStorage.setItem('password', registerValue.password)
+        }        
+        onSuccesfulRegister();
     };
-    const handleOk = () => {
-        setOpen(false);
-    };
-    const handleCancel = () => {
-        setOpen(false);
+
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
     };
 
     return (
-
-            <Modal
-                visible={open}
-                onCancel={handleCancel}
-                onOk={handleOk}
+        <div className={styles.container}>
+            <Form
+                name="basic"
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+                style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
+                <Form.Item<FieldType>
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
 
-            </Modal>
+                <Form.Item<FieldType>
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                    label="Confirm Password"
+                    name="confirmPassword"
+                    rules={[
+                        { required: true, message: 'Please confirm your password!' },
+                        ({ getFieldValue }) => ({
+                            validator(_, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject(new Error('The two passwords do not match!'));
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Register
+                    </Button>
+                </Form.Item>
+
+                <div className={styles.createContainer}>
+                    <Link href="#" onClick={onLoginClick}>
+                        Already have an account?
+                    </Link>
+                </div>
+            </Form>
+        </div>
     );
 };
 
