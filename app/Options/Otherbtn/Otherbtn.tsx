130b3d9eac813b30a33ca500096f664196d943b3
nikushabitskinashvili <bitskinashvili.nika16@gmail.com>
2024-06-24 19:40:29 +0400

    upgrade dark theme

--- app/Options/Otherbtn/Otherbtn.tsx
+++ app/Options/Otherbtn/Otherbtn.tsx
@@ -1,5 +1,7 @@
 
+import { useRecoilState } from "recoil";
 import styles from "./Otherbtn.module.scss";
+import { darkModeState } from "@/app/atoms/states";
 
 type Props = {
     name: string;
@@ -8,12 +10,13 @@ type Props = {
     span ?: string;
 }
 const Otherbtn = (props: Props) => {
+    const [darkMode, setDarkMode] = useRecoilState(darkModeState);
 
 
     return (
 
-        <div className={styles.wrapper}>
-            <div className={styles.flex}>
+        <div className={darkMode ? styles.darkWrapper : styles.wrapper}>
+            <div className={darkMode ? styles.darkFlex : styles.flex}>
                 <img src={props.src} className={styles.global} />
                 <p>{props.name}</p>
             </div>
