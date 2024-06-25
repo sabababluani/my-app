66f16084e4c168a70e33ff5e6426fd996df71180
nikushabitskinashvili <bitskinashvili.nika16@gmail.com>
2024-06-25 21:08:37 +0400

    dasamtavrebelia

--- app/Settings/components/Sidebar/Sidebar.tsx
+++ app/Settings/components/Sidebar/Sidebar.tsx
@@ -2,19 +2,24 @@ import { useRecoilState } from "recoil";
 import styles from "./Sidebar.module.scss";
 import { darkModeState } from "@/app/atoms/states";
 import Link from "next/link";
-import { userIsGeorgianState, profileIsVisibleState, userNameState } from "@/app/atoms/states";
+import { userIsGeorgianState, profileIsVisibleState } from "@/app/atoms/states";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
 
     const [userIsGeorgian] = useRecoilState(userIsGeorgianState);
     
     const [darkMode, setDarkMode] = useRecoilState(darkModeState);
+    const pathName = usePathname();
+    
+    
     return (
         <>
             <div className={darkMode ? styles.darkSidebar : styles.sidebar}>
-                <div className={darkMode ? styles.darkSafe : styles.safe}>
+                <div className={pathName ==='/Otheroptions' ? (darkMode ? styles.darkSafe : styles.safe ): styles.offContainer}>
                     <img src="security.png" alt="" />
-                    <Link href={"/Options"}> {userIsGeorgian ? "კონფიდენციალურობა და უსაფრთხოება" : "Privacy and Security"}</Link>
+
+                    <p> Privacy and Security</p>
                 </div>
                 <div className={darkMode ? styles.darkOption : styles.option}>
                     <img src="option.png" alt="" />
