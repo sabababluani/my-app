a712750b2b9d2e94f16d0491d65be387c0a97747
sabababluani <Saba.bablo@gmail.com>
2024-06-26 01:53:17 +0400

    added date

--- app/History/page.tsx
+++ app/History/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import HistoryDiv from "./components/HistoryDiv/HistoryDiv";
 import styles from './page.module.scss';
+import dayjs from 'dayjs';
 
 interface Task {
     value: string;
@@ -18,10 +19,14 @@ const HistoryPage: React.FC = () => {
         const savedHistory = JSON.parse(localStorage.getItem('historyArray') || '[]') as Task[];
         setHistory(savedHistory);
     }, []);
-
+    const date = dayjs().format('dddd, MMMM D, YYYY');
     return (
         <div className={styles.wrapper}>
-            <div>
+            <div className={styles.wrapperChild}>
+                <div className={styles.dateContainer}> 
+                    <h2>{date}</h2>
+                </div>
+                <hr style={{background : 'white',color : 'white'}}/>
                 <ul className={styles.container}>
                     {history.map((task) => (
                         <li key={task.id}>
